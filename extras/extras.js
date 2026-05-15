e9b60df9cdc73c3f5282f45d272c326750ffdca7
B_Cuvelier <lejoker82@gmail.com>
2026-05-15 19:25:14 +0200

    fix(emargement): @page margin:0, reset body/app, padding 4mm sur le contenu

--- extras/extras.js
+++ extras/extras.js
@@ -467,7 +467,7 @@ function printDeclaration() {
 }
 
 function printEmargement() {
-  injectPageStyle('@page{size:A4 landscape;margin:4mm}');
+  injectPageStyle('@page{size:A4 landscape;margin:0}');
   document.body.setAttribute('data-print', 'emargement');
   window.print();
   cleanupPrint();
